9010d3b5805267f78ef27f9e475281b77b0ddc72
Adib Yusof <adib.yusof@upm.edu.my>
2026-09-01 10:52:56 +0800

    Remove retired course-material archives

--- First Attempts/Week01/.agent/practical/qa/save_practical_outputs.m
+++
@@ -1,5 +0,0 @@
-clearvars
-close all
-livePath = fullfile('/Users/khairuladib/MATLAB Drive/Kuliah/computational-physics-course/Week01','PHY4605_Week01_Practical_Starter.mlx');
-matlab.internal.liveeditor.executeAndSave(livePath)
-disp('LIVE_SCRIPT_EXECUTE_AND_SAVE_COMPLETE')
